1e88bb52a8ce36c132f0a2d68d85668b5c3cbac6
hypax <me@hypax.wtf>
2022-04-22 12:53:01 +0200

    Update metas.php

--- assets/php/metas.php
+++ assets/php/metas.php
@@ -1,5 +1,5 @@
 <meta property="og:site_name" content="Blockarts.de">
 <meta property="og:url" content="https://blockarts.de/">
-<meta property="og:title" content="Hallöchen 👋">
-<meta property="og:image" content="../img/ba-red-bg.jpg">
-<meta property="og:description" content="Pixelschubser">
+<meta property="og:title" content="Blockarts 🔨">
+<meta property="og:image" content="/assets/img/ba-red-bg.jpg">
+<meta property="og:description" content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor">
